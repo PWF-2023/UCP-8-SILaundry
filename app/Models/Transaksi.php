6c317c76be8cf82f78012ccfb0126502339948a6
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2023-07-10 21:16:36 +0700

    membuat model layanan dan transaksi

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer',
+        'user id',
+        'layanan_id',
+        'berat',
+        'total_harga',
+        'is_complete',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function layanan()
+    {
+        return $this->belongsTo(Layanan::class);
+    }
+}
