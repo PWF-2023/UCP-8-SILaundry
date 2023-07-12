6fd0510a36953b6631519a9aeb179131221b752f
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-07-12 14:54:10 +0700

    fixing color for save and cancel in transaction

--- resources/views/transaction/create.blade.php
+++ resources/views/transaction/create.blade.php
@@ -51,14 +51,8 @@
 
                         <div class="flex items-center gap-4">
                             <x-primary-button>{{ __('Save') }}</x-primary-button>
-                            <a href="{{ route('transaction.index') }}"
-                                class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest
-                            text-gray-700 uppercase transition duration-150 ease-in-out
-                            bg-white border border-gray-300 rounded-md shadow-sm dark:bg-gray-800
-                            dark:border-gray-500 dark:text-gray-300 hover:bg-gray-50
-                            dark:hover:bg-gray-700 focus:outline-none focus:ring-2
-                            focus:ring-indigo-500 focus:ring-offset-2
-                            dark:focus:ring-offset-gray-800 disabled:opacity-25">{{ __('Cancel') }}</a>
+
+                            <x-cancel-button href="{{ route('transaction.index') }}" />
                         </div>
                     </form>
 
