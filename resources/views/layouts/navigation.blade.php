8cf7484707cb1f540cbbb7874ce964f6fcdf8d33
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-07-13 20:48:39 +0700

    Edit about blade

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="hover:text-blue-800">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('about')" :active="request()->routeIs('about')">
+                        {{ __('About') }}
+                    </x-nav-link>
                     <x-nav-link :href="route('transaction.index')" :active="request()->routeIs('transaction.index')" class="hover:text-blue-800">
                         {{ __('Transaction') }}
                     </x-nav-link>
@@ -88,6 +91,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('about')" :active="request()->routeIs('about')">
+                {{ __('About') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('transaction.index')" :active="request()->routeIs('transaction.index')">
                 {{ __('Transaction') }}
